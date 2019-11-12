22ccd65946fe7c35162cd91d400cb7ad125cb094
Christin Bergmann <christin.bergmann@Thomass-Air.fritz.box>
2019-11-13 00:46:51 +0100

    fixed filter f sec page content

---
+++ caroussel.js
@@ -0,0 +1,18 @@
+///------------builds carousel and fills content-------------/////
+        let carousel = document.getElementById("carouselID");
+        for (let i = 0; i < result.data.length; i++) {
+            for (let x = 0; x < result.data[i].images.length; x++) {
+                let a = document.createElement("a");
+                a.setAttribute("class", "carousel-item");
+                let p = document.createElement("p");
+                p.setAttribute("class", "nameImg");
+                p.innerHTML = result.data[i].name;
+                let img = document.createElement("img");
+                img.src = result.data[i].images[x].url;
+
+                a.appendChild(img)
+                a.appendChild(p)
+                carousel.appendChild(a)
+            }
+        }
+        M.AutoInit();
